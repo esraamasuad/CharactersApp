ad3f79988382aaa1ae20c2c665908d92b8f5c497
esraa <esraa.g@grinta.ai>
2024-10-19 19:17:21 +0300

    Initial Commit

---
+++ CharactersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CharactersApp
+//
+//  Created by Esraa Gomaa on 10/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
